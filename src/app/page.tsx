b8294e149978566a7ec9061f7cf4d42d7df105f9
technicallyweb3 <mancinoweb3@gmail.com>
2024-07-13 01:58:04 -0600

    working new next app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import {
 import Link from "next/link";
 
 export default function Home() {
-  const { isAuthenticated, user } = useDynamicContext();
+  // const { isAuthenticated, user } = useDynamicContext();
 
   return (
     <DynamicContextProvider
@@ -26,23 +26,20 @@ export default function Home() {
         termsOfServiceUrl: "/terms-of-service",
       }}
     >
-      {isAuthenticated ?
+      {/* {isAuthenticated ? */}
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
           <div className="fixed left-0 top-0 flex w-full justify-center pr-2 pl-2 border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
             <Header />
           </div>
           <div className="fixed bottom-0 left-0 flex h-24 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-            <a
+            <div
               className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
             >
               By{" "}
               <Link href="https://tw3.dev">Technically Web3</Link>
-            </a>
+            </div>
           </div>
         </div>
 
@@ -51,7 +48,7 @@ export default function Home() {
         </div>
 
         <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
             <h2 className="mb-3 text-2xl font-semibold">
               Refer a User{" "}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -99,11 +96,11 @@ export default function Home() {
               Link wallets and add a phone number for even more rewards!
             </p>
           </div>
-          <DynamicWidget buttonContainerClassName="justify-center items-center"/>
+          <DynamicWidget buttonContainerClassName="justify-center items-center" />
         </div>
-        <p>logged in</p>
-      </main> : <p>Logged In</p>
-}
+      </main>
+      {/* : <p>Logged In</p>
+} */}
     </DynamicContextProvider>
   );
 }
